8e0ff8c79e29b2294cb6ef7f2c39b7acd1b5a227
Afeez <harfyzz@gmail.com>
2025-03-04 15:27:36 +0000

    Initial Commit

---
+++ InteractiveOnboarding/InteractiveOnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveOnboardingApp.swift
+//  InteractiveOnboarding
+//
+//  Created by Afeez Yunus on 04/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveOnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
